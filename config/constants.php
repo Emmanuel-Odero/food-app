dcb49b33d8b4fafb81a8e96c48ace8233b226d23
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-04-11 21:23:32 +0300

    finished database contants configurations

--- config/constants.php
+++ config/constants.php
@@ -5,6 +5,6 @@
     define('DB_PASSWORD', '');
 
 
-    $conn = new mysqli(LOCALHOST,DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
+    $conn = new mysqli(LOCALHOST,DB_USERNAME, DB_PASSWORD) or die(mysqli_error());//database connection
     $db_select = new mysqli($conn,'DB_NAME') or die(mysqli_error());
-?>z
+?>
